45574549308c3a39f564fc1f3becc8a1ee2e0b74
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-07-13 20:43:48 +0900

    create profile

--- js/app.js
+++ js/app.js
@@ -22,4 +22,10 @@ function toggleFullScreen() {
   }
 
   toggleFullbtn.addEventListener("click",toggleFullScreen);
+
+  const cursor = document.querySelector('.cursor');
+  document.addEventListener('mousemove',(e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
   

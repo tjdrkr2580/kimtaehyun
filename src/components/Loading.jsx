8c757e0fe144223455a96d511d3d42d756692411
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-16 18:53:46 +0900

    추가 loading animation, home.scss

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../assets/97386129-removebg-preview.png";
 import "./style/loading.css";
-import Loadingbar from "./Loadingbar";
 
 const Loading = () => {
   return (
     <div className="loading-wrapper">
       <section className="loading-box">
         <img className="logo" src={Logo} alt="Logo" />
-        <Loadingbar />
+        <div className="loading-bar">
+          <section className="bar"></section>
+        </div>
       </section>
     </div>
   );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from "react";
 import Loading from "./../components/Loading";
 
 const Home = () => {
-  // const [loading, setLoading] = useState(true);
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setLoading(!loading);
-  //   }, 2500);
-  // }, []);
-  return <Loading />;
-  // {loading ? <Loading /> : <div className="home"></div>}</Loading>;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setInterval(() => {
+      setLoading(!loading);
+    }, 10000);
+  }, []);
+  return <>{loading ? <Loading /> : <div className="home"></div>};</>;
 };
 
 export default Home;
